03238665225bb162fb9724e11632aa815f223f83
Nathan Pickard <nathan.pickard87@gmail.com>
2017-01-02 17:15:33 -0800

    Image loader added to webpack and added site logo

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -37,6 +37,13 @@ module.exports = {
 			'style-loader',
 			'css-loader?sourceMap!sass-loader?sourceMap'	
 			)
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+                'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
         },{
             test: /\.html$/,
             loader: 'html-loader'	
