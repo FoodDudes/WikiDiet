8bf21a27826692e3a75a56ed9d867ac54b378e45
Mugsy Carter <mugsycarter@gmail.com>
2017-01-03 15:13:15 -0800

    added the menu widget to all the views as well as the favorites list with remove and add buttons.

--- app/src/components/favorites/favorites.js
+++ app/src/components/favorites/favorites.js
@@ -5,4 +5,20 @@ export default {
     controller
 };
 
-function controller() {}
+controller.$inject=['userFoodsService'];
+
+function controller(userFoods) {
+    this.removeFromFavorites = ()=>{
+        console.log('remove from favorites clicked');
+    };
+
+    this.addToMenu = ()=>{
+        console.log('add to menu clicked');
+    };
+
+    userFoods.get().then(users => {
+        this.users = users;
+    });
+
+
+}
