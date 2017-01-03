b316b10810cb5147a01d933b24af16b2e58cac2f
JPGothro <jpgothro@gmail.com>
2017-01-03 10:58:37 -0800

    adding models back with correct file names

---
+++ server/lib/models/userFood.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const requiredString = {type: String, required: true};
+
+const userFoodSchema = new Schema({
+    username: requiredString,
+    dietId: {
+        type: Schema.Types.ObjectId,
+        ref: 'DietType'
+    },
+    favorites: [],
+    eaten: [],
+    weight: Number,
+    height: Number,
+    age: Number,
+    gender: String
+});
+
+module.exports = mongoose.model('UserFood', userFoodSchema);
