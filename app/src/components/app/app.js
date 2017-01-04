a54cf897c1799f0e3353bd86a99ecbaefad94902
Mugsy Carter <mugsycarter@gmail.com>
2017-01-04 11:01:13 -0800

    menu now displaying lots of stats and daily totals

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -16,11 +16,16 @@ function controller(userFoods) {
     this.time = dateArr[1];
     //current day
     this.day = dateArr[0];
-    console.log(this.day);
     //get request here to pull all foods from the user with this day as the eaten property and add them to the daily menu
 
     //get this user
     userFoods.get().then(users => {
+        this.totalCalories=0;
+        this.totalSugars=0;
+        this.totalFiber=0;
+        this.totalTotalFats=0;
+        this.totalSaturatedFats=0;
+        this.totalTotalProtein=0;
         this.users = users;
         console.log('this user from app.js:', this.users[2]);
     //populate the menu here
@@ -29,12 +34,20 @@ function controller(userFoods) {
         //pull out only today's meals
         console.log('this.day = ', this.day);
         this.menu = this.eaten.filter((item)=>{
-            console.log('this item is ', item);
-            console.log('this.day is now', this.day);
             return item.day === this.day;
         });
-        console.log('menu is ', this.menu);
 
+        this.menu.forEach((food)=>{
+            this.totalCalories += food.Calories;
+            this.totalSugars += food.sugars;
+            this.totalFiber += food.fiber;
+            this.totalTotalFats += food.totalFats;
+            this.totalSaturatedFats += food.saturatedFats;
+            this.totalTotalProtein += food.totalProtein;
+        });
+
+        console.log('menu is ', this.menu);
+        
     });
 
   
