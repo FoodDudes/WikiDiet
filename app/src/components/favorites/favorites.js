ef596bdc2ad12cd6fd93e225de1bc3c8ee84e64c
Mugsy Carter <mugsycarter@gmail.com>
2017-01-03 17:40:03 -0800

    favorites updated focusing on the add to menu option

--- app/src/components/favorites/favorites.js
+++ app/src/components/favorites/favorites.js
@@ -8,12 +8,24 @@ export default {
 controller.$inject=['userFoodsService'];
 
 function controller(userFoods) {
+    this.menu =[];
+
     this.removeFromFavorites = ()=>{
         console.log('remove from favorites clicked');
     };
 
-    this.addToMenu = ()=>{
-        console.log('add to menu clicked');
+    this.addToMenu = (item)=>{
+        var date = new Date();
+        var datetime = date.toLocaleString();
+        var dateArr = datetime.split(', ');
+        item.time = dateArr[1];
+        item.day = dateArr[0];
+        delete item.$$hashKey;
+        console.log('item is ', item);
+        console.log(this.users[3]);
+        //eventually we want to replace this push with a get request to get all of the eaten foods.  Then push this food into the array.  Then a put request to the userfoods database with the new array.
+        this.menu.push(item);
+        
     };
 
     userFoods.get().then(users => {
