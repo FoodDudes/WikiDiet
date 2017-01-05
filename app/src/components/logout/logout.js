0ca4220366e0d2b47650529521f470c68edb1f08
Mugsy Carter <mugsycarter@gmail.com>
2017-01-05 09:49:42 -0800

    login logout routes now have a variable display.  logout route working

--- app/src/components/logout/logout.js
+++ app/src/components/logout/logout.js
@@ -6,5 +6,14 @@ export default {
     controller
 };
 
-function controller() {
-};
+controller.$inject = ['tokenService', '$state'];
+
+function controller(tokenService, $state) {
+
+    console.log('before clear '+ localStorage.getItem('user')+localStorage.getItem('userFoodUserName'));
+    tokenService.remove();
+    localStorage.removeItem('user');
+    localStorage.removeItem('userFoodUserName');
+    console.log('after clear '+ localStorage.getItem('user')+localStorage.getItem('userFoodUserName'));
+
+}
